58e9dcb5b040df8d951f720ba38ef6669ddfe5a6
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-02-11 23:47:19 -0800

    Initial commit

---
+++ add_surname.py
@@ -0,0 +1,9 @@
+# Author: Kimberly Rojas
+# GitHub username: kimberlyroj
+# Date: 2/11/2022
+# Description: A function named add_surname that takes as a parameter a list of first names.
+def add_surname(first_name_list):
+    full_list = [name+' Kardashian' for name in first_name_list if name[0]=='K']
+    return full_list
+first_name_list = ["Kiki", "Krystal", "Pavel", "Annie", "Koala"]
+print(add_surname(first_name_list))
