35758894fd358ddfa65bfe3aa1129d4149218991
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-02-11 23:57:46 -0800

    2nd nnm

--- add_surname.py
+++ add_surname.py
@@ -2,7 +2,6 @@
 # GitHub username: kimberlyroj
 # Date: 2/11/2022
 # Description: A function named add_surname that takes as a parameter a list of first names.
-
 def add_surname(first_name_list):
     full_list = [name+' Kardashian' for name in first_name_list if name[0]=='K']
 
