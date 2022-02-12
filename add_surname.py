8c07cfab0bf58cd145f1dbb9ba4bb61d81f45e1b
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-02-11 23:56:48 -0800

    2nd nn

--- add_surname.py
+++ add_surname.py
@@ -5,6 +5,9 @@
 
 def add_surname(first_name_list):
     full_list = [name+' Kardashian' for name in first_name_list if name[0]=='K']
+
     return full_list
+
 first_name_list = ["Kiki", "Krystal", "Pavel", "MaryKay", "Annie",  "Koala"]
+
 print(add_surname(first_name_list))
